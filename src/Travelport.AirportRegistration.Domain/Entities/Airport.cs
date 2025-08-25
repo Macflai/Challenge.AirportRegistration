b27c8ce20da85efae0be17843b5cf85eadb09c78
Marti SORIANO <marti.soriano@cotecna.es>
2025-08-25 20:08:54 +0200

    Add Domain entities

---
+++ src/Travelport.AirportRegistration.Domain/Entities/Airport.cs
@@ -0,0 +1,10 @@
+﻿namespace Travelport.AirportRegistration.Domain.Entities;
+
+public class Airport
+{
+    public int Id { get; set; }
+    public string Code { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+
+    public ICollection<Person> People { get; set; } = new List<Person>();
+}
---
+++ src/Travelport.AirportRegistration.Domain/Entities/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace Travelport.AirportRegistration.Domain.Entities;
+
+public class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Surname { get; set; } = string.Empty;
+    public string PassportNumber { get; set; } = string.Empty;
+    public string? Email { get; set; }
+    public string? Phone { get; set; }
+
+    public int AirportId { get; set; }
+    public Airport? Airport { get; set; }
+}
