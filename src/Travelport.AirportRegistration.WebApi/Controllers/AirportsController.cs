3e6f2fe605d1e9c67bc47db8cc32373969ea7904
Marti SORIANO <marti.soriano@cotecna.es>
2025-08-25 19:58:23 +0200

    Initial commit: solution and projects structure with .gitignore

---
+++ src/Travelport.AirportRegistration.WebApi/Controllers/AirportsController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Travelport.AirportRegistration.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AirportsController : ControllerBase
+    {
+ 
+    }
+}
